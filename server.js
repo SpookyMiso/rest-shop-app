7deed6ca2618cbc3bcc1add8f47d55fcebb10c5e
Jenn L <jenn.lazariuk@gmail.com>
2015-08-01 19:47:34 -1000

    App communicating with Api and Jade working

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var jade = require('jade');
+var config = require('./config/config.json');
+var sequelize = require('sequelize');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var restify = require('restify');
+
+var client = restify.createJsonClient({
+  url: 'http://localhost:8080',
+  version: '~1.0'
+});
+
+app.set('views','./views');
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function (req, res) {
+  client.get('http://localhost:8080/products', function (err, _req, _res, obj) {
+    res.render('index');
+    console.log(obj);
+  });
+});
+
+
+
+var server = app.listen(config.port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('weeeeeee', host, port);
+});
+
+
